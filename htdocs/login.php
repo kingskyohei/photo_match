9fcab6cb985ea6589116a0069b9f012e20ef404c
Kyohei Kinugasa <kingskyohei@gmail.com>
2015-12-08 16:56:20 +0900

    commit 20151208

--- htdocs/login.php
+++ htdocs/login.php
@@ -14,12 +14,12 @@ $user_mst_access = new User_Mst_Access();
 /*画面から入力したIDとパスワード*/
 $user_name = $_POST["user_name"];
 $password = $_POST["password"];
+
 // エラーメッセージの初期化
 $errorMessage = "";
 
 // ログインボタンが押された場合
 if (isset($_POST["user_name"])) {
-
   // １．ユーザIDの入力チェック
   if (empty($_POST["user_name"])) {
     $errorMessage = "ユーザIDが未入力です。";
@@ -27,20 +27,16 @@ if (isset($_POST["user_name"])) {
     $errorMessage = "パスワードが未入力です。";
   } 
 
-/*
-  var_dump($_POST["user_name"]);
-  var_dump($_POST["password"]);
-  var_dump($user_name);
-  var_dump($password);
-  */
   // ２．ユーザIDとパスワードが入力されていたら認証する
   if (!empty($_POST["user_name"]) && !empty($_POST["password"])) {
     // mysqlへの接続
     try {
-  		  if($user_mst_access->login($user_name,$password)){
-          $_SESSION["user_name"] = $user_name;
-    	    header("Location: mypage.php");
+        $rtn_id = $user_mst_access->login($user_name,$password);
 
+  		  if(isset($rtn_id)){
+          $_SESSION["user_id"] = $rtn_id;
+          $_SESSION["user_name"] = $user_name;
+     	    header("Location: mypage.php");
         	exit;
         }else{
           // throw new Exception('DBアクセスエラー');
